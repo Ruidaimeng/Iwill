adaf4be77893a390c0ae63392bfd4c0224daae3e
Ruimeng <13816570705@163.com>
2018-05-23 14:07:36 +0800

    第一次使用eclipse提交项目到github

---
+++ src/main/java/com/yunda/gitdemo/IwillApplication.java
@@ -0,0 +1,12 @@
+package com.yunda.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwillApplication.class, args);
+	}
+}
